8053602eef9b7c42806dda3706f2a8baeeb005c3
laurencostello <143658558+laurencostello@users.noreply.github.com>
2026-04-03 18:57:20 +0000

    Deploying to gh-pages from @ laurencostello/laurencostello.github.io@106814bb17176f7cf6dcb5fca5449586339d0cfd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -504,7 +504,7 @@ ninja.data = [{
         title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("laurencostello.bsky.social", "_blank");
+          window.open("https://bsky.app/profile/laurencostello.bsky.social", "_blank");
         },
       },{
         id: 'social-custom_social',
